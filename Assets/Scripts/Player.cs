057f69b87b777fa3a0c60e63731b5ab3146eec24
ZacMansour <47873884+ZacMansour@users.noreply.github.com>
2019-04-12 18:18:54 +1000

    Scripting

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public int health = 100;
+    public float speed = 0.05f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveHorizontal = (Input.GetAxis("Horizontal"));
+        float moveVertical = (Input.GetAxis("Vertical"));
+
+        transform.Translate(new Vector3(moveHorizontal, 0.0f, moveVertical) * speed);
+
+        if(health < 100)
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entered Poison");
+        //Display text saying "Danger"
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        health -= 1;
+        Debug.Log("Losing Health");
     }
 }
