d48a0d6048c656374c99ae492445067cdb098b2f
ZacMansour <47873884+ZacMansour@users.noreply.github.com>
2019-05-02 21:57:00 +1000

    Animation Start

---
+++ Assets/Scripts/DoorControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            anim.SetTrigger("DoorOpen");
+            Debug.Log("Door Open");
+        }
+    }
+
+}
