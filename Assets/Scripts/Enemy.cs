561745462551491f93bc4c1cd6ecb5cb7686f015
ZacMansour <47873884+ZacMansour@users.noreply.github.com>
2019-05-04 19:18:37 +1000

    Animations Complete

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,20 +6,16 @@ public class Enemy : MonoBehaviour
 {
     public int startingHealth = 50;
     public int currentHealth;
+    public float sinkSpeed = 2.5f;
 
+    CapsuleCollider capsuleCollider;
     bool isDead;
+    bool isSinking;
 
-    private void Awake()
+    private void Start()
     {
+        //The enemy's current health 
         currentHealth = startingHealth;
-    }
-
-    public void TakeDamage (int amount, Vector3 hitPoint)
-    {
-        if (isDead)
-        {
-            return;
-        }
 
 
     }
@@ -27,6 +23,47 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //This is how fast the enemy will sink
+        if (isSinking)
+        {
+            transform.Translate(-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
+    }
+
+    public void TakeDamage(int amount, Vector3 hitPoint)
+    {
+        /*If the enemy is dead there is no need to take damage
+        so exit the function*/
+        if (isDead)
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+
+        if(currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+
+        //Turn the collider into a trigger so bullets can pass through
+        capsuleCollider.isTrigger = true;
+    }
+
+    public void StartSinking()
+    {
+        //Find the rigidbody component and make it kinematic
+        GetComponent<Rigidbody>().isKinematic = true;
+
+        //The enemy should sink
+        isSinking = true;
+
+        //After 2 seconds destroy enemy
+        Destroy(gameObject, 2f);
     }
 }
